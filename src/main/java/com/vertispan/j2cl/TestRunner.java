a45edfc13ff4a7d33e14417d4a23e0faafa57fac
Colin Alworth <colin@colinalworth.com>
2019-02-18 17:03:22 -0600

    Actually run test classes in a browser, hardcoded ChromeDriver for the moment

--- src/main/java/com/vertispan/j2cl/TestRunner.java
+++ src/main/java/com/vertispan/j2cl/TestRunner.java
@@ -2,8 +2,18 @@ package com.vertispan.j2cl;
 
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.Duration;
 import java.util.concurrent.ExecutionException;
 
 public class TestRunner {
@@ -18,6 +28,50 @@ public class TestRunner {
             parser.printUsage(System.err);
             System.exit(1);
         }
-        SingleCompiler.run(options.makeOptions());
+
+        Gwt3Options compileOptions = options.makeOptions();
+        SingleCompiler.run(compileOptions);
+
+        // write a simple html file to that output dir
+        compileOptions.getJsOutputFile();
+        new File(compileOptions.getJsOutputFile()).getParent();
+        Path junitStartupFile = Paths.get(new File(compileOptions.getJsOutputFile()).getParent(), "junit.html");
+        Files.copy(TestRunner.class.getResourceAsStream("/junit.html"), junitStartupFile, StandardCopyOption.REPLACE_EXISTING);
+        String startupHtmlFile = junitStartupFile.toAbsolutePath().toString();
+
+
+        // assuming that was successful, start htmlunit to run the test
+        WebDriver driver = null;
+        try {
+            driver = new ChromeDriver();
+            driver.get("file://" + startupHtmlFile);
+
+            // loop and poll if tests are done
+            new FluentWait<>(driver)
+                    .withTimeout(Duration.ofMinutes(1))
+                    .withMessage("Tests failed to finish in timeout")
+                    .pollingEvery(Duration.ofMillis(100))
+                    .until(d -> isFinished(d));
+
+            // check for success
+            if (!isSuccess(driver)) {
+                System.err.println("At least one test failed, please try manually");
+            } else {
+                System.err.println("Tests passed!");
+            }
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+
+    }
+
+    private static boolean isSuccess(WebDriver d) {
+        return (Boolean) ((JavascriptExecutor) d).executeScript("return window.G_testRunner.isSuccess()");
+    }
+
+    private static boolean isFinished(WebDriver d) {
+        return (Boolean) ((JavascriptExecutor) d).executeScript("return !!(window.G_testRunner && window.G_testRunner.isFinished())");
     }
 }
